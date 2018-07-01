33ac91b7e9b450f37dc8c28069fcd7c76accc5e7
Lucas Mafra <lucas.mafra95@gmail.com>
2018-07-01 10:19:39 -0300

    scroll

--- src/App.js
+++ src/App.js
@@ -9,22 +9,27 @@ import About from './components/About/About'
 import Location from './components/Location/Location'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
-import './App.css';
+import './App.css'
+import { Element , scrollSpy } from 'react-scroll'
 
 class App extends Component {
+    componentDidMount() {
+        scrollSpy.update();
+    }
+
     render() {
         return (
             <div className="App">
                 <NavBar/>
-                <Banner/>
-                <Activities/>
-                <Expo/>
-                <CuboAccess/>
-                <Sponsors/>
-                <About/>
-                <Location/>
-                <Contact/>
-                <Footer/>
+                <Element name={"banner"}><Banner/></Element>
+                <Element name={"activities"}><Activities/></Element>
+                <Element name={"expo"}><Expo/></Element>
+                <Element name={"cubo-access"}><CuboAccess/></Element>
+                <Element name={"sponsors"}><Sponsors/></Element>
+                <Element name={"about"}><About/></Element>
+                <Element name={"location"}><Location/></Element>
+                <Element name={"contact"}><Contact/></Element>
+                <Element name={"footer"}><Footer/></Element>
             </div>
     );
   }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './NavBar.css'
 import logo from './cubo-deitado.png'
+import { Link } from 'react-scroll'
 const classNames = require('classnames');
 
 class NavBar extends Component {
@@ -39,15 +40,75 @@ class NavBar extends Component {
         return (
             <div className={classNames('navbar', { 'transparent': this.state.transparent })}>
                 <div className="navbar__logo">
-                    <img src={logo} alt='cubo-deitado' />
+                    <Link
+                        activeClass="active"
+                        to="banner"
+                        spy={true}
+                        smooth={true}
+                        offset={-80}
+                        duration={500}>
+                        <img src={logo} alt='cubo-deitado' />
+                    </Link>
                 </div>
                 <div className="navbar__content">
-                    <a className="hover-underline-animation">Programação</a>
-                    <a className="hover-underline-animation">Painéis Científicos</a>
-                    <a className="hover-underline-animation">Sobre o CUBO</a>
-                    <a className="hover-underline-animation">Como Chegar?</a>
-                    <a className="hover-underline-animation">Fale Conosco</a>
-                    <a className="hover-underline-animation">Meu Cubo</a>
+                    <Link
+                        activeClass="active"
+                        to="activities"
+                        spy={true}
+                        smooth={true}
+                        offset={-80}
+                        duration={500}
+                        className="hover-underline-animation"
+                    >
+                        Programação
+                    </Link>
+                    <Link
+                        activeClass="active"
+                        to="expo"
+                        spy={true}
+                        smooth={true}
+                        offset={-160}
+                        duration={500}
+                        className="hover-underline-animation"
+                    >
+                        Painéis Científicos
+                    </Link>
+                    <Link
+                        activeClass="active"
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        offset={-120}
+                        duration={500}
+                        className="hover-underline-animation"
+                    >
+                        Sobre o CUBO
+                    </Link>
+                    <Link
+                        activeClass="active"
+                        to="location"
+                        spy={true}
+                        smooth={true}
+                        offset={-160}
+                        duration={500}
+                        className="hover-underline-animation"
+                    >
+                        Como Chegar?
+                    </Link>
+                    <Link
+                        activeClass="active"
+                        to="contact"
+                        spy={true}
+                        smooth={true}
+                        offset={-80}
+                        duration={500}
+                        className="hover-underline-animation"
+                    >
+                        Fale Conosco
+                    </Link>
+                    <a className="hover-underline-animation">
+                        Meu Cubo
+                    </a>
                 </div>
             </div>
         )
