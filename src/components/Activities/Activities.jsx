4658b45a1fb91c248aa906e4d8b42261fc4b443a
Lucas Mafra <lucas.mafra95@gmail.com>
2018-07-02 00:09:13 -0300

    applying classes

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -1,37 +1,63 @@
 import React from 'react'
 import './Activities.css'
+import classNames from 'classnames';
 
-const Activities = () => (
-    <div id="activities" className="activities">
-        <div className="activities__section palestras">
-            <div className="activities__section__title">
-                <h1>Palestras</h1>
+class Activities extends React.Component {
+
+    state = {
+        selected: undefined
+    }
+
+    select(type) {
+        this.setState({
+            selected: type
+        })
+    }
+
+    render() {
+        return (
+            <div id="activities" className="activities">
+                <div className={classNames("activities__section", { selected: this.state.selected === 'palestras', nonSelected: this.state.selected !== 'palestras' && this.state.selected})} onClick={() => this.select('palestras')}>
+                    <div className="palestras">
+                        <div className="activities__section__title">
+                            <h1>Palestras</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={classNames("activities__section", { selected: this.state.selected === 'workshops', nonSelected: this.state.selected !== 'workshops' && this.state.selected})} onClick={() => this.select('workshops')}>
+                    <div className="workshops">
+                        <div className="activities__section__title">
+                            <h1>Workshops</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={classNames("activities__section", { selected: this.state.selected === 'handsOn', nonSelected: this.state.selected !== 'handsOn' && this.state.selected})} onClick={() => this.select('handsOn')}>
+                    <div className="hands-on">
+                        <div className="activities__section__title">
+                            <h1>Hands-on</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={classNames("activities__section", { selected: this.state.selected === 'cirurgias', nonSelected: this.state.selected !== 'cirurgias' && this.state.selected})} onClick={() => this.select('cirurgias')}>
+                    <div className="cirurgias">
+                        <div className="activities__section__title">
+                            <h1>
+                                Cirurgias <br/>
+                                ao vivo
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={classNames("activities__section", { selected: this.state.selected === 'stands', nonSelected: this.state.selected !== 'stands' && this.state.selected})} onClick={() => this.select('stands')}>
+                    <div className="stands">
+                        <div className="activities__section__title">
+                            <h1>Stands</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div className="activities__section workshops">
-            <div className="activities__section__title">
-                <h1>Workshops</h1>
-            </div>
-        </div>
-        <div className="activities__section hands-on">
-            <div className="activities__section__title">
-                <h1>Hands-on</h1>
-            </div>
-        </div>
-        <div className="activities__section cirurgias">
-            <div className="activities__section__title">
-                <h1>
-                    Cirurgias <br/>
-                    ao vivo
-                </h1>
-            </div>
-        </div>
-        <div className="activities__section stands">
-            <div className="activities__section__title">
-                <h1>Stands</h1>
-            </div>
-        </div>
-    </div>
-)
+        )
+    }
+}
 
 export default Activities
