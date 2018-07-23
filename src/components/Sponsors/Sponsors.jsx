20124c95d67b551e217d6a443fd2b494732efe15
Lucas Mafra <lucas.mafra95@gmail.com>
2018-07-22 22:38:52 -0300

    updated sponsors

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Sponsors.css'
-import septodont from './logo_septodont.png'
-import quinelato from './logo_quinelato.jpg'
-import sisconsultoria from './logo_sis_consultoria.png'
-import tdv from './logo_tdv.png'
+import dentsply from './dentsply.png'
+import fgm from './fgm.png'
+import morelli from './morelli.png'
+import odontoPrev from './odonto-prev.png'
+import quinelato from './quinelato.png'
+import septodont from './septodont.png'
+import sis from './sis.png'
+import tdv from './tdv.png'
+import valda from './valda.png'
 import { Link } from 'react-scroll'
 
 const Sponsors = () => (
@@ -13,23 +18,48 @@ const Sponsors = () => (
         </div>
         <div className="sponsors__grid">
             <div className="sponsors__grid__card">
+                <a href="http://www.dentsply.com.br" target="_blank" rel="noopener noreferrer">
+                    <img alt="sponsor" src={dentsply} className="grow"/>
+                </a>
+            </div>
+            <div className="sponsors__grid__card">
+                <a href="http://www.fgm.ind.br/site/" target="_blank" rel="noopener noreferrer">
+                    <img alt="sponsor" src={fgm} className="grow"/>
+                </a>
+            </div>
+            <div className="sponsors__grid__card">
+                <a href="https://www.morelli.com.br/" target="_blank" rel="noopener noreferrer">
+                    <img alt="sponsor" src={morelli} className="grow"/>
+                </a>
+            </div>
+            <div className="sponsors__grid__card">
+                <a href="http://www.odontoprev.com.br/" target="_blank" rel="noopener noreferrer">
+                    <img alt="sponsor" src={odontoPrev} className="grow"/>
+                </a>
+            </div>
+            <div className="sponsors__grid__card">
+                <a href="https://www.quinelato.com.br" target="_blank" rel="noopener noreferrer">
+                    <img alt="sponsor" src={quinelato} className="grow"/>
+                </a>
+            </div>
+            <div className="sponsors__grid__card">
                 <a href="http://www.septodont.com.br/" target="_blank" rel="noopener noreferrer">
                     <img alt="sponsor" src={septodont} className="grow"/>
                 </a>
             </div>
             <div className="sponsors__grid__card">
-                <a href="https://www.quinelato.com.br/" target="_blank" rel="noopener noreferrer">
-                    <img alt="sponsor" src={quinelato} className="grow"/>
-                </a>
-            </div>
-            <div className="sponsors__grid__card">
                 <a href="http://www.sisconsultoria.net/" target="_blank" rel="noopener noreferrer">
-                    <img alt="sponsor" src={sisconsultoria} className="grow"/>
+                    <img alt="sponsor" src={sis} className="grow"/>
                 </a>
             </div>
             <div className="sponsors__grid__card">
                 <a href="http://www.tdv.com.br/" target="_blank" rel="noopener noreferrer">
                     <img alt="sponsor" src={tdv} className="grow"/>
+                </a>
+            </div>
+            <div className="sponsors__grid__card">
+                <a target="_blank" rel="noopener noreferrer">
+                    <img alt="sponsor" src={valda} className="grow"/>
                 </a>
             </div>
         </div>
