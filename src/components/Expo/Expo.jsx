224064139a7731fe1664f3ec5bba12f7b751ffca
Lucas Mafra <lucas.mafra95@gmail.com>
2018-07-23 22:23:08 -0300

    removed prices

--- src/components/Expo/Expo.jsx
+++ src/components/Expo/Expo.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Expo.css'
 import Button from '../Button/Button'
-import cadeira from './cadeira.png'
-import fotopolimerizador from './fotopolimerizador.png'
+// import cadeira from './cadeira.png'
+// import fotopolimerizador from './fotopolimerizador.png'
 import ScrollAnimation from 'react-animate-on-scroll'
 
 const Expo = () => (
@@ -17,10 +17,9 @@ const Expo = () => (
             <div className="expo__content">
                 <div className="expo__content-judging">
                     <span>Para alunos da graduação e pós graduação</span>
-                    <div>
+                    <div style={{ alignSelf: 'flex-end' }}>
                         <ul>
-                            Com banca avaliadora formada por<br/>
-                            professores da USP nas categorias:
+                            Com banca avaliadora formada por professores da USP nas categorias:
                         </ul>
                         <li>
                             <h4>Clínico</h4>
@@ -40,35 +39,35 @@ const Expo = () => (
                     </div>
                 </div>
                 <div className="expo__content-prizes">
-                    <ul>
-                        <span>
-                            Confira alguns dos prêmios para os melhores colocados!
-                        </span>
-                        <li>
-                            <div>
-                                <h4>LEC Fotopolimerizador</h4>
-                            </div>
-                            <div>
-                                <img alt="Fotopolimerizador" src={fotopolimerizador}/>
-                            </div>
-                        </li>
-                        <li>
-                            <div>
-                                <h4>Kit Dentsply</h4>
-                            </div>
-                            <div>
-                                <img alt="" src={cadeira}/>
-                            </div>
-                        </li>
-                        <li>
-                            <div>
-                                <h4>Brindes Curaprox</h4>
-                            </div>
-                            <div>
-                                <img alt="" src={cadeira}/>
-                            </div>
-                        </li>
-                    </ul>
+                    {/*<ul>*/}
+                        {/*<span>*/}
+                            {/*Confira alguns dos prêmios para os melhores colocados!*/}
+                        {/*</span>*/}
+                        {/*<li>*/}
+                            {/*<div>*/}
+                                {/*<h4>LEC Fotopolimerizador</h4>*/}
+                            {/*</div>*/}
+                            {/*<div>*/}
+                                {/*<img alt="Fotopolimerizador" src={fotopolimerizador}/>*/}
+                            {/*</div>*/}
+                        {/*</li>*/}
+                        {/*<li>*/}
+                            {/*<div>*/}
+                                {/*<h4>Kit Dentsply</h4>*/}
+                            {/*</div>*/}
+                            {/*<div>*/}
+                                {/*<img alt="" src={cadeira}/>*/}
+                            {/*</div>*/}
+                        {/*</li>*/}
+                        {/*<li>*/}
+                            {/*<div>*/}
+                                {/*<h4>Brindes Curaprox</h4>*/}
+                            {/*</div>*/}
+                            {/*<div>*/}
+                                {/*<img alt="" src={cadeira}/>*/}
+                            {/*</div>*/}
+                        {/*</li>*/}
+                    {/*</ul>*/}
                 </div>
                 <a
                     href="https://goo.gl/forms/snfrbfQcTO3pGJOM2"
