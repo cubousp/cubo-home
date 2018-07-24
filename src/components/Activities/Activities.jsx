7e745c002a3f03de18a4b1fb9310050fb8c20a17
Lucas Mafra <lucas.mafra95@gmail.com>
2018-07-24 01:24:40 -0300

    adapted banner

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Activities.css'
 import Slider from 'react-slick'
+import Hidden from '@material-ui/core/Hidden';
 
 class Activities extends React.Component {
     ACTIVITIES = {
@@ -30,40 +31,98 @@ class Activities extends React.Component {
 
         return (
             <div>
-                <Slider  {...settings} afterChange={(index) => this.setState({current: index})}>
-                    <div className={"activities__section"}>
-                        <div className="palestras">
-                            <div className="activities__section__title">
-                                <h1>Palestras e Workshops</h1>
-                                <h6>Os melhores profissionais do mercado compartilhando conhecimento técnico e inovador.</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={"activities__section"}>
-                        <div className="hands-on">
-                            <div className="activities__section__title">
-                                <h1>Hands-on</h1>
-                                <h6>Atividades conectando o mundo acadêmico com a prática na Odontologia.</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={"activities__section"}>
-                        <div className="cirurgias">
+                <Hidden mdDown>
+                    <Slider  {...settings} afterChange={(index) => this.setState({current: index})}>
+                        <div className={"activities__section"}>
+                            <div className="palestras">
                                 <div className="activities__section__title">
-                                    <h1>Cirurgias ao vivo</h1>
-                                    <h6>Aprendendo com as técnicas mais modernas do mercado e casos clínicos diferenciados.</h6>
+                                    <h1>Palestras e Workshops</h1>
+                                    <h6>Os melhores profissionais do mercado compartilhando conhecimento técnico e inovador.</h6>
                                 </div>
-                        </div>
-                    </div>
-                    <div className={"activities__section"}>
-                        <div className="stands">
-                            <div className="activities__section__title">
-                                <h1>Stands</h1>
-                                <h6>Stands de produtos com ofertas e brindes exclusivos para os participantes.</h6>
                             </div>
                         </div>
-                    </div>
-                </Slider>
+                        <div className={"activities__section"}>
+                            <div className="hands-on">
+                                <div className="activities__section__title">
+                                    <h1>Hands-on</h1>
+                                    <h6>Atividades conectando o mundo acadêmico com a prática na Odontologia.</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"activities__section"}>
+                            <div className="cirurgias">
+                                    <div className="activities__section__title">
+                                        <h1>Cirurgias ao vivo</h1>
+                                        <h6>Aprendendo com as técnicas mais modernas do mercado e casos clínicos diferenciados.</h6>
+                                    </div>
+                            </div>
+                        </div>
+                        <div className={"activities__section"}>
+                            <div className="stands">
+                                <div className="activities__section__title">
+                                    <h1>Stands</h1>
+                                    <h6>Stands de produtos com ofertas e brindes exclusivos para os participantes.</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </Slider>
+                </Hidden>
+                <Hidden mdUp>
+                    <Slider  {...settings} arrows={false} dots={false} afterChange={(index) => this.setState({current: index})}>
+                        <div className={"activities__section"}>
+                            <div className="palestras">
+                                <div className="activities__section__title">
+                                    <h1 style={{ fontSize: 24 }}>Palestras e <br/>Workshops</h1>
+                                    <h6 style={{
+                                        marginTop: 32,
+                                        textAlign: 'start',
+                                        fontSize: 16,
+                                        maxWidth: 260
+                                    }}>Os melhores profissionais do mercado compartilhando conhecimento técnico e inovador.</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"activities__section"}>
+                            <div className="hands-on">
+                                <div className="activities__section__title">
+                                    <h1 style={{ fontSize: 24 }}>Hands-on</h1>
+                                    <h6 style={{
+                                        marginTop: 32,
+                                        textAlign: 'start',
+                                        fontSize: 16,
+                                        maxWidth: 260
+                                    }}>Atividades conectando o mundo acadêmico com a prática na Odontologia.</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"activities__section"}>
+                            <div className="cirurgias">
+                                <div className="activities__section__title">
+                                    <h1 style={{ fontSize: 24 }}>Cirurgias ao vivo</h1>
+                                    <h6 style={{
+                                        marginTop: 32,
+                                        textAlign: 'start',
+                                        fontSize: 16,
+                                        maxWidth: 260
+                                    }}>Aprendendo com as técnicas mais modernas do mercado e casos clínicos diferenciados.</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"activities__section"}>
+                            <div className="stands">
+                                <div className="activities__section__title">
+                                    <h1 style={{ fontSize: 24 }}>Stands</h1>
+                                    <h6 style={{
+                                        marginTop: 32,
+                                        textAlign: 'start',
+                                        fontSize: 16,
+                                        maxWidth: 260
+                                    }}>Stands de produtos com ofertas e brindes exclusivos para os participantes.</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </Slider>
+                </Hidden>
             </div>
         );
     }
